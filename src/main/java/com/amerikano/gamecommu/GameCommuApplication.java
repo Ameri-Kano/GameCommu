7fdc4b46273d6d6c86d5ece8513246722fb5b567
Ameri-Kano <yeokkim13@naver.com>
2023-07-16 23:09:19 +0900

    Initial commit

---
+++ src/main/java/com/amerikano/gamecommu/GameCommuApplication.java
@@ -0,0 +1,13 @@
+package com.amerikano.gamecommu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCommuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GameCommuApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/amerikano/gamecommu/GameCommuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amerikano.gamecommu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCommuApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
