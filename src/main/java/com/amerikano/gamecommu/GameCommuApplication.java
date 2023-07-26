567c5565c3bcd73ce17acb35b4a3ff59605de7d6
Ameri-Kano <yeokkim13@naver.com>
2023-07-27 01:26:47 +0900

    FEAT : 유저 기능 추가 구현
    유저 정보 수정, 탈퇴(토큰 필요)
    토큰이 필요한 기능에 필터 적용(UserFilter) 했으나 추가 수정 예정

--- src/main/java/com/amerikano/gamecommu/GameCommuApplication.java
+++ src/main/java/com/amerikano/gamecommu/GameCommuApplication.java
@@ -2,10 +2,12 @@ package com.amerikano.gamecommu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ServletComponentScan
 public class GameCommuApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/amerikano/gamecommu/domain/dto/user/UpdateUserForm.java
+++ src/main/java/com/amerikano/gamecommu/domain/dto/user/UpdateUserForm.java
@@ -1,19 +1,21 @@
 package com.amerikano.gamecommu.domain.dto.user;
 
-import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
-public class JoinForm {
+public class UpdateUserForm {
 
   private String email;
-  private String password;
   private String name;
-  private LocalDate birth;
+  private String birth;
   private String phone;
 
 }
